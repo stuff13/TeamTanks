f81692014ebede18012d8b41f331a5be5781ebdf
Michael MacKinnon <mackinnon.m@gmail.com>
2016-04-17 23:53:20 +0100

    updated to multithreaded object to listen for client

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using UnityEngine;
+
+public class NetworkController : MonoBehaviour
+{
+
+    public static NetworkController Instance { get; private set; }
+	// Use this for initialization
+	void Start ()
+    {
+	    if (Instance != null)
+	    {
+	        Destroy(Instance.gameObject);
+	    }
+
+	    Instance = this;
+
+	    if (SystemInfo.operatingSystem.Contains("Windows"))
+	    {
+            new PacketListener();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void UpdateObjectLocations(GameObject currentGameObject)
+    {
+        // go through list of clients
+        // update each one with new Location of gameObject
+    }
+
+    // some way to connect from client
+
+    // some way to receive messages from clients: figure out new location of objects 
+}
+
+[Serializable]
+public struct Packet
+{
+    public Vector3 Location;    // current Location
+    public Vector4 Rotation;    // current orientation
+    public Vector3 Velocity;    // current Velocity <== can I use this if I don't have full synchronisation on client and server
+    public String Name;
+}
+
---
+++ Assets/Scripts/PacketListener.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+
+public class PacketListener
+{
+    private volatile bool _keepListeningForClients = true;
+
+    public PacketListener()
+    {
+        // create thread
+        var listenerThread = new BackgroundWorker();
+        listenerThread.DoWork += Listen;
+        listenerThread.WorkerReportsProgress = true;
+        listenerThread.ProgressChanged += ProgressMessage;
+        // send it off
+        listenerThread.RunWorkerAsync();
+        Debug.Log("Started background thread to listen for messages");
+    }
+
+    private void ProgressMessage(object sender, ProgressChangedEventArgs eventArgs)
+    {
+        Debug.Log((string)eventArgs.UserState);
+    }
+
+    private void Listen(object sender, DoWorkEventArgs eventArgs)
+    {
+        BackgroundWorker worker = sender as BackgroundWorker;
+
+        // Data buffer for incoming data.
+        var bytes = new byte[1024];
+
+        // Establish the local endpoint for the socket.
+        IPAddress ipAddress = IPAddress.Parse("192.168.0.6");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        EndPoint senderRemote = localEndPoint;
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        try
+        {
+            socket.Bind(localEndPoint);
+
+            while (_keepListeningForClients)
+            {
+                Debug.Log("Waiting to receive datagrams from client...");
+                Debug.Log("Waiting to receive datagrams from client...");
+
+                // Start an asynchronous socket to listen for connections.
+                socket.ReceiveFrom(bytes, ref senderRemote);
+
+                Debug.Log("Received message from client. Decoding...");
+                String message = Encoding.Default.GetString(bytes);
+                if (worker != null)
+                    worker.ReportProgress(0, message);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+        finally
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+
+    public void RequestStopListening()
+    {
+        _keepListeningForClients = false;
+    }
+
+    //public static int Main(String[] args)
+    //{
+    //    var listener = new SocketListener();
+
+    //    Console.WriteLine("\nPress ENTER to continue...");
+    //    Console.Read();
+    //    return 0;
+    //}
+}
