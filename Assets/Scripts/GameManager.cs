81b1423687395eec78f4612380fd87898a38fe76
Michael MacKinnon <mackinnon.m@gmail.com>
2016-04-19 00:09:30 +0100

    WIP: update client and server to submit locations of objects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour 
 {
 	public static GameManager Instance;
 	[SerializeField] private GameObject mainCamera = null;
 	[SerializeField] private GameObject turretCamera = null;
-//	[SerializeField] private GameObject turret = null;
-	[SerializeField] private GameObject gun = null;
-//	[SerializeField] private float maxTurretRotation = 120.0f;
-//	[SerializeField] private float maxGunAngle = 50.0f;   // angle from horizon
-//	[SerializeField] private float turretSpeed = 2.0f;
-	[SerializeField] private float gunSpeed = 5.0f;
-	private CardboardHead head;
+    //	[SerializeField] private GameObject turret = null;
+    //	[SerializeField] private float maxTurretRotation = 120.0f;
+    //	[SerializeField] private float maxGunAngle = 50.0f;   // angle from horizon
+    //	[SerializeField] private float turretSpeed = 2.0f;
+
 	private bool onApple = false;
 
-	void Start () 
+    public static bool IsServer { get { return !Instance.onApple; } }
+    public CardboardHead Head { get; set; }
+
+    void Start () 
 	{
 		if(Instance == null)
 		{
@@ -33,7 +35,7 @@ public class GameManager : MonoBehaviour
 	        Cardboard.Create();
 	        turretCamera.SetActive(true);
 	        mainCamera.SetActive(false);
-	        head = turretCamera.GetComponent<StereoController>().Head;
+            Head = turretCamera.GetComponent<StereoController>().Head;
 	        GetComponent<Cardboard>().VRModeEnabled = true;
 	    }
 	    else
@@ -46,11 +48,6 @@ public class GameManager : MonoBehaviour
 
 	void Update () 
 	{
-		if(onApple)
-		{
-			var targetQ = Quaternion.LookRotation(head.Gaze.direction);
-			gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, targetQ, Time.deltaTime * gunSpeed);
-		}
 	}
 
 }
--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -1,31 +1,45 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 
 using UnityEngine;
 
-public class NetworkController : MonoBehaviour
+public class NetworkController : MonoBehaviour, IUpdateClientObjects
 {
 
     public static NetworkController Instance { get; private set; }
 
-    private PacketListener listener;
-	// Use this for initialization
-	void Start ()
+    private PacketServer listener;
+    private PacketClient sender;
+
+    private GameObject gun;
+    private GameObject tank;
+
+    // Use this for initialization
+    void Start ()
     {
+        // set up as a singleton
 	    if (Instance != null)
 	    {
 	        Destroy(Instance.gameObject);
 	    }
-
 	    Instance = this;
 
 	    if (SystemInfo.operatingSystem.Contains("Windows"))
 	    {
-            listener = new PacketListener();
+            listener = new PacketServer();
         }
-        else
-        {
-        	new PacketSender();
+        else // we're on an apple device
+	    {
+            tank = GameObject.Find("Tank");
+	        gun = GameObject.Find("Gun");
+	        var packet = new Packet
+	                         {
+	                             Location = gun.transform.position,
+                                 Rotation = gun.transform.rotation
+	                         };
+        	sender = new PacketClient(packet, this);
         }
     }
 	
@@ -34,10 +48,21 @@ public class NetworkController : MonoBehaviour
 	
 	}
 
-    public void UpdateObjectLocations(GameObject currentGameObject)
+    void FixedUpdate()
     {
         // go through list of clients
         // update each one with new Location of gameObject
+    }
+
+    public void UpdateObjectLocations(GameObject currentGameObject)
+    {
+        var packet = new Packet
+                         {
+                             Location = currentGameObject.transform.position,
+                             Rotation = currentGameObject.transform.rotation,
+                         };
+
+        listener.SendPacket(packet);
     }
 
     // some way to connect from client
@@ -46,16 +71,63 @@ public class NetworkController : MonoBehaviour
 
     void OnApplicationQuit()
     {
-        listener.RequestStopListening();
+        if (listener != null)
+        {
+            listener.RequestStopListening();
+        }
+
+        
+    }
+
+
+    // these are the updates from the server
+    public void UpdateClient(Packet updatedGun)
+    {
+        tank.transform.position = updatedGun.Location;
+        tank.transform.rotation = updatedGun.Rotation;
     }
 }
 
-[Serializable]
+
+// 11 * 4 = 44 bytes
+// max size UDP message can send and ensure it isn't broken up is 508 bytes
+//      => we can 11 Packet objects at once
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct Packet
 {
+    public int ObjectId;    // id mapped to name of object
     public Vector3 Location;    // current Location
-    public Vector4 Rotation;    // current orientation
     public Vector3 Velocity;    // current Velocity <== can I use this if I don't have full synchronisation on client and server
-    public String Name;
+    public Quaternion Rotation;    // current orientation
+
+    public static byte[] ToBytes(Packet packet)
+    {
+        int size = Marshal.SizeOf(packet);
+        byte[] arr = new byte[size];
+
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        Marshal.StructureToPtr(packet, ptr, true);
+        Marshal.Copy(ptr, arr, 0, size);
+        Marshal.FreeHGlobal(ptr);
+        return arr;
+    }
+
+    public static Packet FromBytes(byte[] arr)
+    {
+        var packet = new Packet();
+        int size = Marshal.SizeOf(packet);
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        Marshal.Copy(arr, 0, ptr, size);
+
+        packet = (Packet)Marshal.PtrToStructure(ptr, packet.GetType());
+        Marshal.FreeHGlobal(ptr);
+
+        return packet;
+    }
+}
+
+public interface IUpdateClientObjects
+{
+    void UpdateClient(Packet thing);
 }
 
